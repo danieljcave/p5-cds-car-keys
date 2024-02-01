d6a94514b8b45aee8571162b0fab8ea0bbb0c640
Dan Cave <danielcave99@gmail.com>
2024-02-01 15:03:38 +0000

    Initial Commit

---
+++ cds_car_keys/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for cds_car_keys project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'cds_car_keys.settings')
+
+application = get_asgi_application()
