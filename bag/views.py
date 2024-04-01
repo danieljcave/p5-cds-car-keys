94787b5a181d85697777b362d2d5ade7cdad5531
Daniel Cave <49564558+danieljcave@users.noreply.github.com>
2024-04-01 20:05:43 +0100

    Updated toasts with viewable bag for user to view bag while searching products

--- bag/views.py
+++ bag/views.py
@@ -16,6 +16,7 @@ def add_to_bag(request, item_id):
 
     if item_id in list(bag.keys()):
         bag[item_id] += quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag[item_id] = quantity
         messages.success(request, f'Added {product.name} to your bag')
@@ -33,8 +34,10 @@ def adjust_bag(request, item_id):
 
     if quantity > 0:
         bag[item_id] = quantity
+        messages.success(request, f'Updated {product.name} quantity to {bag[item_id]}')
     else:
         bag.pop(item_id, None)
+        messages.success(request, f'Removed {product.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -49,9 +52,11 @@ def remove_from_bag(request, item_id):
         
         if item_id in bag:
             del bag[item_id]
+            messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
