187f9098aba8e1611075d9c4105e9fdc097ca1f7
Dan Cave <danielcave99@gmail.com>
2024-04-07 23:11:35 +0000

    added employee form and views

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .models import Employee
+from .forms import EmployeeForm
+from django.contrib.admin.views.decorators import staff_member_required
 
-# Create your views here.
+def index(request):
+    employees = Employee.objects.all()
+    return render(request, 'index.html', {'employees': employees})
+
+@staff_member_required
+def add_employee(request):
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, request.FILES)
+        if form.is_valid():
+            form.save()
+            return redirect('index')
+    else:
+        form = EmployeeForm()
+    return render(request, 'add_employee.html', {'form': form})
