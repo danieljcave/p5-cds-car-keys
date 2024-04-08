562debdb971cddb93db770ff2089766e740d16a1
Daniel Cave <49564558+danieljcave@users.noreply.github.com>
2024-04-08 01:47:16 +0100

    add update and delete employee data

--- employees/urls.py
+++ employees/urls.py
@@ -4,4 +4,6 @@ from . import views
 urlpatterns = [
     path('employees/', views.employee_list, name='employee_list'),
     path('add_employee/', views.add_employee, name='add_employee'),
+    path('edit_employee/<int:employee_id>/', views.edit_employee, name='edit_employee'),
+    path('delete_employee/<int:employee_id>/', views.delete_employee, name='delete_employee'),
 ]
--- employees/views.py
+++ employees/views.py
@@ -1,19 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.contrib import messages
 from .models import Employee
 from .forms import EmployeeForm
+from django.contrib.admin.views.decorators import staff_member_required
 
 def employee_list(request):
     employees = Employee.objects.all()
     return render(request, 'employee/employee_list.html', {'employees': employees})
 
+@staff_member_required
 def add_employee(request):
     if request.method == 'POST':
         form = EmployeeForm(request.POST, request.FILES)
         if form.is_valid():
             form.save()
             messages.success(request, 'Successfully added employee!')
-            return redirect('index')  # Redirect to the index page after adding an employee
+            return redirect('employee_list')  # Redirect to the index page after adding an employee
         else:
             messages.error(request, 'Failed to add employee. Please ensure the form is valid.')
     else:
@@ -22,3 +24,32 @@ def add_employee(request):
     template = 'employee/add_employee.html'
     context = {'form': form}
     return render(request, template, context)
+
+
+@staff_member_required
+def edit_employee(request, employee_id):
+    employee = get_object_or_404(Employee, id=employee_id)
+    if request.method == 'POST':
+        form = EmployeeForm(request.POST, request.FILES, instance=employee)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Employee Updated!')
+            return redirect('employee_list')
+    else:
+        form = EmployeeForm(instance=employee)
+
+    template = 'employee/edit_employee.html'
+    context = {
+        'form': form,
+        'employee': employee,
+    }
+
+    return render(request, template, context)
+
+
+@staff_member_required
+def delete_employee(request, employee_id):
+    employee = get_object_or_404(Employee, id=employee_id)
+    employee.delete()
+    messages.success(request, 'Employee deleted!')
+    return redirect(reverse('employee_list'))
