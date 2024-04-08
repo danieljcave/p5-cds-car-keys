7db19cf5c61f6bfc695fcaf0caae7c55bab4208f
Daniel Cave <49564558+danieljcave@users.noreply.github.com>
2024-04-08 04:22:59 +0100

    added services add edit and delete

--- services/urls.py
+++ services/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.service_list, name='service_list'),
+    path('services/', views.service_list, name='service_list'),
     path('add_service/', views.add_service, name='add_service'),
     path('edit/<int:service_id>/', views.edit_service, name='edit_service'),
     path('delete/<int:service_id>/', views.delete_service, name='delete_service'),
