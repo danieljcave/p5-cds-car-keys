2ecf15a8a0ead7995da00bbb2abd7102ced07fcc
Daniel Cave <49564558+danieljcave@users.noreply.github.com>
2024-04-08 02:11:46 +0100

    remove newsletter

--- newsletter/models.py
+++
@@ -1,6 +0,0 @@
-from django.db import models
-
-class Newsletter(models.Model):
-    subject = models.CharField(max_length=200)
-    content = models.TextField()
-    scheduled_send_date = models.DateField()
